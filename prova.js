27d29a58480a18a0ffac7b06a1c793c4d2d38ed1
Simone <simone.gentile@gamil.com>
2024-10-26 14:25:30 +0200

    compito terminato

---
+++ prova.js
@@ -0,0 +1,9 @@
+//Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato..
+const f=function(){
+let l1=2
+let l2=4
+const area=l1*l2
+console.log("ecco la tua area",area)
+return area
+}
+f()
